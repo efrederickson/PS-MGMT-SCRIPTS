7681737224f25aa1fde6949e9d7ab1112c822307
Administrator@SHADOWSTALKERS.LAB <Administrator@SHADOWSTALKERS.LAB>
2019-09-18 13:53:20 -0400

    Add VSCode task

---
+++ tasks/vscode.ps1
@@ -0,0 +1,20 @@
+New-Task -Name vscode -ScriptBlock {
+    # This should install VSCode 
+
+    # Map drive
+    net use X: "\\ad\acdc utt" /user:student password
+
+    # Run installer
+    x:\VSCodeUserSetup-x64-1.38.1.exe /verysilent
+
+    # Wait for installer (since it detaches from console)
+    PS C:\Users\Administrator.AD1> while (Get-Process -Name "VSCodeUserSetup-x64-1.38.1" -ErrorAction SilentlyContinue) { 
+        Start-Sleep -Seconds 5 
+    }
+
+    # Remove drive
+    net use /del x:
+
+    # Kill hanging processes
+    Get-Process -Name Code -ErrorAction SilentlyContinue | Stop-Process -Force
+}
